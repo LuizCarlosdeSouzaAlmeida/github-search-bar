46d1755434e5e11e360efbfe7982fe15e2c69cb9
LuizCarlosdeSouzaAlmeida <luizmk_rulez@live.com>
2022-04-25 11:54:38 -0300

    feat: color and svg assets

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,16 @@ const { accounts, selectedAccount } = storeToRefs(main);
     </v-main>
   </v-app>
 </template>
+
+<style>
+:root {
+  --dark: #200F21;
+  --white: #F0F0F0;
+  --primary: #382039;
+  --secondary: #5A3D5C;
+  --secondary-1: #FD71EA;
+  --purple: #A51C92;
+  --gradient: linear-gradient(90deg, #A51C92 0%, #F638DC 100%);
+  --gradient_white: linear-gradient(359.39deg, #EFEEEE 0.46%, #FFFFFF 99.42%);
+}
+</style>
